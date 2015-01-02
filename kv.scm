35f1581cbc8ab0c9a523f1b946cc88a4a51689ee
Adrian Kummerlaender <knairda@t-online.de>
2015-01-02 23:04:14 +0100

    Reactivated `write` functionality * i.e. writing to a store now requires explicit mention of the `write` command keyword

--- kv.scm
+++ kv.scm
@@ -15,11 +15,16 @@
 (define commands (list
   (make-command "show"
                 (lambda (arguments)
-                 (let ((count (length arguments)))
-                  (cond ((= 0 count) (print (stores->print (read-all-stores))))
-                        ((= 1 count) (print (path->store   (file-in-base   (first arguments)))))
-                        ((= 2 count) (print (entry-value   (entry-of-store (path->store (file-in-base (first arguments))) (second arguments)))))
-                        (else        (print "show: too many arguments"))))))))
+                  (let ((count (length arguments)))
+                    (cond ((= 0 count) (print (stores->print (read-all-stores))))
+                          ((= 1 count) (print (path->store   (file-in-base   (first arguments)))))
+                          ((= 2 count) (print (entry-value   (entry-of-store (path->store (file-in-base (first arguments))) (second arguments)))))
+                          (else        (print "show: too many arguments"))))))
+  (make-command "write"
+                (lambda (arguments)
+                  (if (>= (length arguments) 3)
+                    (write-entry (path->store (file-in-base (first arguments))) (second arguments) (drop arguments 2))
+                    (print "write: storage, key and value required"))))))
 
 (define (name->command-implementation name)
   (let ((command (find (lambda (command)
