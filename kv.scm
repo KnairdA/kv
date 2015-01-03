be5ddf2f92c9bd27c9c2d491cd806de5db9cd5c5
Adrian Kummerlaender <knairda@t-online.de>
2015-01-03 10:45:01 +0100

    Extracted command implementations into separate functions * defining them inline as lambda expressions what quite unclear

--- kv.scm
+++ kv.scm
@@ -12,19 +12,20 @@
        (remove (lambda (x) (directory? (file-in-base x)))
                (directory base))))
 
-(define commands (list
-  (make-command "show"
-                (lambda (arguments)
-                  (let ((count (length arguments)))
-                    (cond ((= 0 count) (print (stores->print (read-all-stores))))
-                          ((= 1 count) (print (path->store   (file-in-base   (first arguments)))))
-                          ((= 2 count) (print (entry-value   (entry-of-store (path->store (file-in-base (first arguments))) (second arguments)))))
-                          (else        (print "show: too many arguments"))))))
-  (make-command "write"
-                (lambda (arguments)
-                  (if (>= (length arguments) 3)
-                    (write-entry (path->store (file-in-base (first arguments))) (second arguments) (drop arguments 2))
-                    (print "write: storage, key and value required"))))))
+(define (perform-show arguments)
+  (let ((count (length arguments)))
+    (cond ((= 0 count) (print (stores->print (read-all-stores))))
+          ((= 1 count) (print (path->store   (file-in-base   (first arguments)))))
+          ((= 2 count) (print (entry-value   (entry-of-store (path->store (file-in-base (first arguments))) (second arguments)))))
+          (else        (print "show: too many arguments")))))
+
+(define (perform-write arguments)
+  (if (>= (length arguments) 3)
+    (write-entry (path->store (file-in-base (first arguments))) (second arguments) (drop arguments 2))
+    (print "write: storage, key and value required")))
+
+(define commands (list (make-command "show"  perform-show)
+                       (make-command "write" perform-write)))
 
 (define (name->command-implementation name)
   (let ((command (find (lambda (command)
