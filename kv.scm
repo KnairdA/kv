770fc366839102a70ce7b27d51201b617b26e165
Adrian Kummerlaender <knairda@t-online.de>
2015-01-09 20:40:01 +0100

    Included documentation into command list

--- kv.scm
+++ kv.scm
@@ -3,17 +3,17 @@
 
 (define base "~/.kv/")
 
-(define-record command name implementation)
+(define-record documentation arguments description)
+(define-record command       name implementation documentation)
 
 (define (perform-help arguments)
   (let ((printer (make-key-value-pretty-printer 0 30)))
     (print (format #f "~{~&~A~}"
-                   (map (lambda (pair) (printer (first pair) (second pair)))
-                        (list (list "show [STORE [KEY]]"           "prints stores, keys of stores or values of keys")
-                              (list "all  [STORE [KEY]]"           "prints key value pairs of every store, a single store or a single key")
-                              (list "write STORE KEY VALUE"        "writes VALUE to KEY in STORE")
-                              (list "delete STORE KEY"             "deletes KEY in STORE")
-                              (list "rename STORE OLD-KEY NEW-KEY" "renames OLD-KEY of STORE to NEW-KEY")))))))
+                   (map (lambda (command)
+                          (let ((documentation (command-documentation command)))
+                            (printer (documentation-arguments   documentation)
+                                     (documentation-description documentation))))
+                        commands)))))
 
 (define (perform-show arguments)
   (let ((count (length arguments)))
@@ -47,12 +47,31 @@
                         (rename-entry (second arguments) (third arguments)))
     (print "rename: storage, old-key and new-key required")))
 
-(define commands (list (make-command "help"   perform-help)
-                       (make-command "show"   perform-show)
-                       (make-command "all"    perform-all)
-                       (make-command "write"  perform-write)
-                       (make-command "delete" perform-delete)
-                       (make-command "rename" perform-rename)))
+(define commands
+  (list (make-command "help"
+                      perform-help
+                      (make-documentation "help"
+                                          "displays this message"))
+        (make-command "show"
+                      perform-show
+                      (make-documentation "show [STORE [KEY]]"
+                                          "prints stores, keys of stores or values of keys"))
+        (make-command "all"
+                      perform-all
+                      (make-documentation "all [STORE [KEY]]"
+                                          "prints key value pairs of every store, a single store or a single key"))
+        (make-command "write"
+                      perform-write
+                      (make-documentation "write STORE KEY VALUE"
+                                          "writes VALUE to KEY in STORE"))
+        (make-command "delete"
+                      perform-delete
+                      (make-documentation "delete STORE KEY"
+                                          "deletes KEY in STORE"))
+        (make-command "rename"
+                      perform-rename
+                      (make-documentation "rename STORE OLD-KEY NEW-KEY"
+                                          "renames OLD-KEY of STORE to NEW-KEY"))))
 
 (define (name->command-implementation name)
   (let ((command (find (lambda (command)
